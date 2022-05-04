af348257e1197540c22886af42f97b609b8b10cf
Minjung Kwon <minjk121@gmail.com>
2022-05-04 11:35:49 -0400

    working on getting device types with ip_addr.txt

---
+++ IP_addr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# this code creates and saves the MAC addr and device name into MAC_addr.txt file
+
+# sudo nmap -sn 10.49.30.241/24 | grep MAC | awk '{print $3","$4","$5}' > MAC_addr.txt
+
+IP=$(hostname -I)  # gets IP addr of wifi (wlan0)
+#echo ${IP// /}/24
+
+#sudo nmap -sn ${IP// /}/24 | grep MAC | awk '{print $3","$4","$5}' > MAC_addr.txt
+#cat MAC_addr.txt
+
+#for ip in (ip_addr.txt)
+cat ip_addr.txt | while read ip
+do
+	sudo nmap -O -v $ip > result.txt
+done
+
+cat result.txt
+	

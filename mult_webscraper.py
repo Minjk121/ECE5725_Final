1fba67b04d991265e59abe84813a9d31e5df5a05
Minjung Kwon <Minjk121@gmail.com>
2022-05-10 17:18:07 -0400

    Updated multiple webscraper

--- mult_webscraper.py
+++ mult_webscraper.py
@@ -1,5 +1,5 @@
 '''
-webscraper that gets data from the multiple mrtg websites
+webscraper that gets data & images from the multiple mrtg websites
 '''
 
 import pandas as pd
@@ -9,13 +9,16 @@ import requests
 # returns tuple of daily average (in, out)
 def daily_in_out(url):
     # page to scrape
-    # url = 'http://mrtg.cit.cornell.edu/switch/WorkDir/duffield1-5400.21.html'
     request = requests.get(url, timeout=5)
     soup = BeautifulSoup(request.text, features="html.parser")
 
     in_lst = []
     out_lst = []
+    mrtg = ""
+    port = ""
 
+    for item in soup.find("font", string=lambda text: "|" in text):
+        port = item.split("|")[-1]
     for item in soup.findAll("tr", {"class":"in"}):
         for i in item.findAll("td"):
             if 'Traffic' not in i.text:
@@ -25,30 +28,50 @@ def daily_in_out(url):
             if 'Traffic' not in i.text:
                 out_lst.append(i.text)
 
-    day_avg_in = float(in_lst[1].split()[0])
-    day_avg_out = float(out_lst[1].split()[0])
+    for item in soup.findAll("img", {"alt":"day"}):
+        # print(item["src"])
+        mrtg = item["src"]
 
-    return (day_avg_in, day_avg_out)
+    day_cur_in = float(in_lst[2].split()[0])
+    day_cur_out = float(out_lst[2].split()[0])
+
+    return (day_cur_in, day_cur_out, mrtg, port)
 
 def main():
-    urls = ['http://mrtg.cit.cornell.edu/switch/WorkDir/duffield1-5400.21.html',
-    'http://mrtg.cit.cornell.edu/switch/WorkDir/duffield1-5400.49.html']
+    urls = ['http://mrtg.cit.cornell.edu/switch/WorkDir/phillips1-5400.252.html',
+            'http://mrtg.cit.cornell.edu/switch/WorkDir/duffield2-5400.120.html',
+            'http://mrtg.cit.cornell.edu/switch/WorkDir/phillips2-5400.132.html',
+            'http://mrtg.cit.cornell.edu/switch/WorkDir/phillips2-5400.131.html'
+            # '',
+            # '',
+            # '',
+            # '',
+            # '',
+            # '',
+            # '',
+            # '',
+            # ''
+            ]
     
-    device_lst = []
+    port_lst = []
     in_lst = []
     out_lst = []
+    mrtg_lst = []
 
     for url in urls: 
-        device_lst.append(url.split('/')[-1].replace(".html",""))
         result = daily_in_out(url)
+        port_lst.append(result[3])
         in_lst.append(result[0])
         out_lst.append(result[1])
+        mrtg_lst.append(result[2])
 
     df = pd.DataFrame(
-        {'Device Name': device_lst,
+        {'Port Name': port_lst,
          'In': in_lst,
-         'Out': out_lst
+         'Out': out_lst,
+         'Graph': mrtg_lst
         })
+
     print(df)
 if __name__ == "__main__":
     main()

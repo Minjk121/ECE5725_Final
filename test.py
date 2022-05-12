6921921f62f77650150521df134cdeb518cfa6cb
Minjung Kwon <Minjk121@gmail.com>
2022-05-12 17:10:07 -0400

    Fixing pygame.error

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
-import mult_webscraper
+# import mult_webscraper
+import RPi.GPIO as GPIO
+import time
+import sys
 
+import pygame
+from pygame.locals import * # for event MOUSE variables
+import os
 
-d = mult_webscraper.main()
-print(d)
-print(type(d))
+# d = mult_webscraper.main()
+# print(d)
+# print(type(d))
+
+infoObject = pygame.display.Info()
+print(infoObject.current_w, infoObject.current_h)
+# pygame.display.set_mode((infoObject.current_w, infoObject.current_h))

bc0dfb7e034553b640b79b14c8c8d7be20997488
arisdaemon <18351249+arisdaemon@users.noreply.github.com>
2022-05-17 19:31:36 -0400

    added button functionality (six buttons)

--- mirror_display.py
+++ mirror_display.py
@@ -19,7 +19,15 @@ end_time = t0 + 600 # changed timeout to 10 min
 update_time = t0 + 300 # update traffic rates by 5 min
 
 GPIO.setmode(GPIO.BCM)
+# exit/panic button; the button closest to the motor driver
+GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+# menu buttons (on the piTFT)
+GPIO.setup(27, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+# "main menu" button (on the breadboard, closest to the cobbler)
+GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 # initialize pygame for piTFT 
 pygame.init()
@@ -48,6 +56,9 @@ space_list={'Duffield atrium':'1','ECE lounge':'1','Upson 2nd floor':'1','Upson 
 space_list_pos={1:(0.521*screen_width,0.093*screen_height),2:(0.521*screen_width,0.278*screen_height),3:(0.521*screen_width,0.463*screen_height),4:(0.521*screen_width,0.648*screen_height),5:(0.521*screen_width,0.833*screen_height)}
 # space_list_pos={1:(1000,100),2:(1000,300),3:(1000,500),4:(1000,700),5:(1000,900)}
 # space_list_pos={'1':(1000,100),'2':(1000,300),'3':(1000,500),'4':(1000,700),'5':(1000,900), 'main menu': (1500, 1000)}
+
+# need this for remote control/button functionality
+recommended_spaces_list = [] 
 
 # congestion_data contains study spaces + halls
 congestion_df = mult_webscraper.main() # data frame
@@ -127,10 +138,11 @@ def updateSurfaceAndRect(buttons):
 # we will only consider the top five least congested areas for study space recommendations
 # default ordering is in alphabetical order, such that popular spaces like CIS lounge, ECE lounge, and Duffield
 # always show up near the top (if all else are equal)
-# function takes no inputs, has no outputs
+# function takes no inputs, return top tier list
 def updateSurfaceAndRect_StudySpace():
     #space_list_ordered = sorted(space_list,key=space_list.get)
     index = 1
+    recommended_spaces = []
     for space, v in sorted(space_list.items()):
         if (index < 6):
             congestion_colors = ['green','yellow','red']
@@ -141,12 +153,15 @@ def updateSurfaceAndRect_StudySpace():
             screen.blit(text_surface, rect)
             menu_buttons_rect[space] = rect
             index += 1
+            recommended_spaces.append(space)
     
     # draw main menu button
     text_surface = create_text_box('main menu', WHITE, SKYBLUE, 50, 50)
     rect = text_surface.get_rect(center=congestion_menu['main menu'])
     screen.blit(text_surface, rect)
     menu_buttons_rect['main menu'] = rect
+
+    return recommended_spaces
 
 
 # helper function to determine the route from outside of the engineering quad buildings to a specified study "space"
@@ -240,7 +255,7 @@ def updateScreen(route=[]):
         updateSurfaceAndRect(congestion_menu)
     elif menu_level == 3: # study spaces
         determine_congestion_level()
-        updateSurfaceAndRect_StudySpace()
+        recommended_spaces_list = updateSurfaceAndRect_StudySpace()
 
     elif menu_level == 5:
         # map is shown properly on monitor
@@ -249,7 +264,6 @@ def updateScreen(route=[]):
         screen.blit(campus_map, (0,0))
         #TODO: determine_route(space) needs to be stuck in somewhere based on what's clicked in menu level 3
         # until then, here's a placeholder variable for route
-        route = determine_route(my_text)
         draw_route_on_map(route)
         
     pygame.display.flip()
@@ -342,12 +356,58 @@ while (time.time() < end_time):
                         # menu_buttons['study spaces'] = menu_buttons.pop('main menu')
                         # newText = my_text
                         # newRect = rect
-                        updateScreen()
+                        route = determine_route(my_text)
+                        updateScreen(route)
                         break
                     if (my_text=='quit'):
                         sys.exit()
             # del menu_buttons_rect[tempText]
             # menu_buttons_rect[newText] = newRect
-        
-    if ( not GPIO.input(17) ):
+    
+    # for button presses, when doing "remote control" of the menu
+    # if in congestion map, the buttons are top-to-bottom sequential
+    if (menu_level == 2):
+        menu_level = 4 #go to dashboard
+        if ( not GPIO.input(26) ): # Phillips
+            drawDashboard("Phillips")
+            updateScreen()
+            break
+        if ( not GPIO.input(13 )): # Duffield
+            drawDashboard("Duffield")
+            updateScreen()
+            break
+        if ( not GPIO.input(13 )): # Upson
+            drawDashboard("Upson")
+            updateScreen()
+            break
+        if ( not GPIO.input(13 )): #Rhodes
+            drawDashboard("Rhodes")
+            updateScreen()
+            break
+    if (menu_level == 3):
+        menu_level = 5 #go to routing
+        if ( not GPIO.input(26) ):
+            route = determine_route(recommended_spaces_list[0])
+            updateScreen(route)
+            break
+        if ( not GPIO.input(13 )):
+            route = determine_route(recommended_spaces_list[1])
+            updateScreen(route)
+            break
+        if ( not GPIO.input(13 )):
+            route = determine_route(recommended_spaces_list[2])
+            updateScreen(route)
+            break
+        if ( not GPIO.input(13 )):
+            route = determine_route(recommended_spaces_list[3])
+            updateScreen(route)
+            break
+    
+    # clicked main menu button
+    if ( not GPIO.input(23) ):
+        menu_level = 1
+        updateScreen()
+        break
+    # panic button
+    if ( not GPIO.input(13) ):
         sys.exit()

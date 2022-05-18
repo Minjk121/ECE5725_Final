de87ee1084f9274e30f1fc2b50cd9bca7ac938c1
Minjung Kwon <Minjk121@gmail.com>
2022-05-17 20:25:21 -0400

    Added df for in_out rate for daily

--- mult_webscraper.py
+++ mult_webscraper.py
@@ -9,23 +9,28 @@ import requests
 # import pandas as pd
 # # from pandas.plotting import table
 # import numpy as np
-from bokeh.io import export_png, export_svgs
-from bokeh.models import ColumnDataSource, DataTable, TableColumn
+import matplotlib
+# import seaborn as sns
 
-def save_df_as_image(df, path):
-    source = ColumnDataSource(df)
-    df_columns = [df.index.name]
-    df_columns.extend(df.columns.values)
-    columns_for_table=[]
-    for column in df_columns:
-        columns_for_table.append(TableColumn(field=column, title=column))
-
-    data_table = DataTable(source=source, columns=columns_for_table,height_policy="auto",width_policy="auto",index_position=None)
-    export_png(data_table, filename = path)
-
-# def get_in_out_df_by_port(hall_name):
+# def get_in_out_df_by_port(df, png):
+#     filter = df['Graph'] == png
+#     filtered_df = df.loc[filter]
+#     # print(filtered_df)
+#     congestion_data[spaces] = f
 #     port_df = 
 #     return port_df
+
+# def save_df_as_image(df, path="dashboard_img.png"):
+    # Set background to white
+    # norm = matplotlib.colors.Normalize(-1,1)
+    # colors = [[norm(-1.0), "white"],
+    #         [norm( 1.0), "white"]]
+    # cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", colors)
+    # # Make plot
+    # plot = sns.heatmap(df, annot=True, cmap=cmap, cbar=False)
+    # fig = plot.get_figure()
+    # fig.savefig(path)
+
 # def save_df_as_image(df, path="dashboard_img.png"):
 #     df_styled = df.style.background_gradient() #adding a gradient based on values in cell
 #     dfi.export(df_styled, path)
@@ -57,7 +62,6 @@ def daily_in_out(url):
         # print(item["src"])
         mrtg = item["src"]
 
-    # print(in_lst[2])
     day_cur_in = float(in_lst[2].split()[0])
     day_cur_out = float(out_lst[2].split()[0])
 
@@ -68,7 +72,7 @@ def daily_in_out(url):
 
     # print(day_cur_in)
 
-    return (day_cur_in, day_cur_out, mrtg, port)
+    return (day_cur_in, day_cur_out, mrtg, port, in_lst, out_lst)
 
 # convert df to dictionary of congestion_data
 def convert_df_to_dict(df):
@@ -95,8 +99,7 @@ def convert_df_to_graph_lst(df, hall_name):
     filtered_df = df.loc[filter]
     graph_lst = list(filtered_df['Graph'])
     return graph_lst
-    
-    
+        
 def convert_url_to_df(urls):
 
     name_lst = []
@@ -105,6 +108,10 @@ def convert_url_to_df(urls):
     in_lst = []
     out_lst = []
     mrtg_lst = []
+
+    info_lst = []
+    daily_in = []
+    daily_out=[]
 
     for url, location in urls: 
         # hall_name = url.split('/')[-1].split('-')[0]
@@ -117,8 +124,12 @@ def convert_url_to_df(urls):
         in_lst.append(result[0])
         out_lst.append(result[1])
         mrtg_lst.append(result[2])
+        daily_in.append(result[4][0:3])
+        daily_out.append(result[5][0:3])
+        info_lst.append(['Daily Max','Daily Average', 'Current'])
 
-    df = pd.DataFrame(
+
+    df_whole = pd.DataFrame(
         {'Port Name': port_lst,
          'Hall Name': hall_lst,
          'Location Name': name_lst,
@@ -126,7 +137,20 @@ def convert_url_to_df(urls):
          'Out': out_lst,
          'Graph': mrtg_lst
         })
-    return df
+    df_daily_in_out = pd.DataFrame(
+        {'Hall Name': hall_lst,
+         'Info':info_lst,
+         'In': daily_in,
+         'Out': daily_out
+        })
+    # df_daily_in_out.swapaxes("index", "columns")
+    return df_whole, df_daily_in_out
+
+def in_out_by_hall(df, hall_name):
+    filter = df['Hall Name'] == hall_name.lower()
+    filtered_df = df.loc[filter]
+    return filtered_df[['Info','In','Out']].swapaxes("index", "columns")
+
 def main():
     urls = [('http://mrtg.cit.cornell.edu/switch/WorkDir/phillips1-5400.252.html', 'ECE lounge'),
             ('http://mrtg.cit.cornell.edu/switch/WorkDir/duffield2-5400.120.html', 'Duffield atrium'), # near phillips
@@ -147,11 +171,22 @@ def main():
             ('http://mrtg.cit.cornell.edu/switch/WorkDir/rhodes5-1-5400.25.html', 'Rhodes 5th floor'),
             ]
 
-    df = convert_url_to_df(urls)
+    df = convert_url_to_df(urls)[0]
     # print(df)
+    df = convert_url_to_df(urls)[1]
+    # print(df)
+    print(in_out_by_hall(df, "upson"))
+    
     # print(convert_df_to_graph_lst(df, 'Upson'))
-    save_df_as_image(df)
-    return df
+    # save_df_as_image(df)
+    '''
+    filter = df['Hall Name'] == 'duffield'
+    filtered_df = df.loc[filter]
+    filtered_df.append
+    new_df = filtered_df[[,'In','Out']]
+    print(new_df)
+    '''
+    return convert_url_to_df(urls)[0], convert_url_to_df(urls)[1]
     
     # data = convert_df_to_dict(df)
     # print(data)

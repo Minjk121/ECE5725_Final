f57dcfb4b101d0fa1776e2422ca66ad34ae52430
Minjung Kwon <Minjk121@gmail.com>
2022-05-17 23:42:41 -0400

    Debug after reinstalling 1.9.6

--- mirror_display.py
+++ mirror_display.py
@@ -250,7 +250,8 @@ def drawDashboard(hall_name):
     file.write(response.content)
     file.close()
     mrtg_graph = pygame.image.load('sample_image.png')
-    screen.blit(mrtg_graph, (int(screen_width/8), int(screen_height/8) * count))
+    # screen.blit(mrtg_graph, (int(screen_width/8), int(screen_height/8) * count))
+    screen.blit(mrtg_graph, 0,0)
         # draw tables
         
 

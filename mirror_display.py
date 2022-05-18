56865d4648f08ed472fc7a1ecd16e4360a4cac82
Minjung Kwon <Minjk121@gmail.com>
2022-05-18 00:36:47 -0400

    Fixing graph orientations

--- mirror_display.py
+++ mirror_display.py
@@ -284,7 +284,7 @@ def updateScreen(route=[]):
         #     # draw mrtg graphs
             mrtg_graph = pygame.image.load("./img/"+images)
             if count >= 6:
-                screen.blit(mrtg_graph, (int(screen_width/8)*4, int(screen_height/6) * count_2))
+                screen.blit(mrtg_graph, (int(screen_width/8)*4+10, int(screen_height/6) * count_2))
                 count_2+=1
             else:
                 screen.blit(mrtg_graph, (int(screen_width/8), int(screen_height/6) * count))

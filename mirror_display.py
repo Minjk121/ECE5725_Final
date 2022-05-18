5800e66eb11bd33913ca1b15512f34bb749a14e9
Minjung Kwon <Minjk121@gmail.com>
2022-05-18 00:55:35 -0400

    Fixed string orientation on dashboard

--- mirror_display.py
+++ mirror_display.py
@@ -290,11 +290,11 @@ def updateScreen(route=[]):
 
             if count >= 6:
                 screen.blit(mrtg_graph, (int(screen_width/8)*4+30, int(screen_height/6) * count_2))
-                screen.blit(text_surface, (int(screen_width/8)*4+30, int(screen_height/6) * count_2+30))
+                screen.blit(text_surface, (int(screen_width/8)*4+30, int(screen_height/6) * count_2-30))
                 count_2+=1
             else:
                 screen.blit(mrtg_graph, (int(screen_width/8), int(screen_height/6) * count))
-                screen.blit(text_surface, (int(screen_width/8), int(screen_height/6) * count+30))
+                screen.blit(text_surface, (int(screen_width/8), int(screen_height/6) * count-30))
             count+=1
 
     elif menu_level == 5:
